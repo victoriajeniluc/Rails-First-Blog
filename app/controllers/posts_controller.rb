544f9ff05a6dd2fdfac2df8cd4d4974a59f714e1
Victoria Luc <victoriajeniluc@gmail.com>
2017-10-29 17:31:36 -0500

    Create an action for Edit and Delete Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,28 @@ class PostsController < ApplicationController
 	def show 
 		@post = Post.find(params[:id])
 	end 
+
+	def edit
+		@post = Post.find(params[:id])
+	end 
+
+	def update 
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else 
+			@errors = @post.errors.full_messages
+			render 'edit'
+		end 
+	end 
 	
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end 
+
 	# rails 4 has a strong parameter where you have explicity state what you want to allow.. so you have to make a private method 
 	private 
 		def post_params 
